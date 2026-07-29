a77cebc9a616965253d54d13c4b640b590716ad1
BrianPicado <bpicado50144@ufide.ac.cr>
2026-07-28 23:19:34 -0600

    parte brian

---
+++ Proyecto_Introduccion_Programacion_JN/src/main/java/com/mycompany/proyecto_introduccion_programacion_jn/Estudiante.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_introduccion_progra;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brian
+ */
+public class Estudiante {
+    
+    private int cedula;
+    private String nombre;
+    private String correo;
+
+    public Estudiante() {
+        cedula = 0;
+        nombre = "";
+        correo = "";
+    }
+
+    public Estudiante(int cedula, String nombre, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public void registrarEstudiante() {
+
+        String cedulaString;
+
+        cedulaString = JOptionPane.showInputDialog(
+                null,"Ingrese la cédula del estudiante:"
+        );
+
+        cedula = Integer.parseInt(cedulaString);
+
+        nombre = JOptionPane.showInputDialog(
+                null,"Ingrese el nombre del estudiante:"
+        );
+
+        correo = JOptionPane.showInputDialog(
+                null,"Ingrese el correo del estudiante:"
+        );
+
+        JOptionPane.showMessageDialog(
+                null,"Estudiante registrado correctamente."
+        );
+    }
+
+    public String mostrarDatos() {
+
+        String informacion;
+
+        informacion = "Cédula: " + cedula
+                + "\nNombre: " + nombre
+                + "\nCorreo: " + correo;
+
+        return informacion;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
---
+++ Proyecto_Introduccion_Programacion_JN/src/main/java/com/mycompany/proyecto_introduccion_programacion_jn/MenuPrincipal.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.proyecto_introduccion_progra;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brian
+ */
+public class MenuPrincipal{
+
+   private Estudiante estudiantes[] = new Estudiante[10];
+   private Materia materias[] = new Materia[10];
+ 
+   private int cantidadEstudiantes = 0;
+   private int cantidadMaterias = 0;
+
+   public void mostrarMenu() {
+       int opcion;
+       do {
+           opcion = Integer.parseInt(JOptionPane.showInputDialog(null,"SISTEMA DE NOTAS ESTUDIANTIL\n\n"
+                + "1. Registrar estudiante\n"
+                + "2. Registrar materia\n"
+                + "3. Registrar notas\n"
+                + "4. Registrar asistencia\n"
+                + "5. Registrar justificación\n"
+                + "6. Calcular promedio\n"
+                + "7. Verificar estado del estudiante\n"
+                + "8. Verificar derecho a reposición\n"
+                + "9. Generar reporte\n"
+                + "10. Salir\n\n"
+                + "Seleccione una opción:")
+            );
+
+            switch (opcion) {
+
+                case 1:
+                    registrarEstudiante();
+                    break;
+
+                case 2:
+                    registrarMateria();
+                    break;
+
+                case 3:
+                    
+                    break;
+
+                case 4:
+                    
+                    break;
+
+                case 5:
+                    
+                    break;
+
+                case 6:
+                    calcularPromedio();
+                    break;
+
+                case 7:
+                    
+                    break;
+                    
+                case 8:
+                   
+                    break;
+
+                case 9:
+                    
+                    break;
+
+                case 10:
+                    
+                    break;
+
+                default:
+                    
+                    break;
+            }
+
+        } while (opcion != 10);
+    }
+
+    public void registrarEstudiante() {
+
+        if (cantidadEstudiantes < estudiantes.length) {
+            
+            estudiantes[cantidadEstudiantes] = new Estudiante();
+
+            estudiantes[cantidadEstudiantes].registrarEstudiante();
+
+            cantidadEstudiantes++;
+
+        } else {
+
+            JOptionPane.showMessageDialog(
+                    null,"No se pueden registrar más estudiantes."
+            );
+        }
+    }
+
+public void registrarMateria() {
+
+    if (cantidadEstudiantes == 0) {
+
+        JOptionPane.showMessageDialog(
+                null,"Primero debe registrar un estudiante."
+        );
+
+    } else if (cantidadMaterias < materias.length) {
+
+        int cedulaBuscada;
+
+        cedulaBuscada = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        null,"Ingrese la cédula del estudiante:"
+                )
+        );
+
+        Estudiante estudianteEncontrado;
+
+        estudianteEncontrado = buscarEstudiante(
+                cedulaBuscada
+        );
+
+        if (estudianteEncontrado != null) {
+
+            materias[cantidadMaterias] = new Materia();
+
+            materias[cantidadMaterias].registrarMateria(
+                    estudianteEncontrado
+            );
+
+            cantidadMaterias++;
+
+        } else {
+
+            JOptionPane.showMessageDialog(
+                    null,"No existe un estudiante con esa cédula."
+            );
+        }
+
+    } else {
+
+        JOptionPane.showMessageDialog(
+                null,"No se pueden registrar más materias."
+        );
+    }
+}
+public Estudiante buscarEstudiante(int cedulaBuscada) {
+
+    Estudiante estudianteEncontrado = null;
+
+    for (int i = 0; i < cantidadEstudiantes; i++) {
+
+        if (estudiantes[i].getCedula() == cedulaBuscada) {
+            estudianteEncontrado = estudiantes[i];
+        }
+    }
+
+    return estudianteEncontrado;
+}
+    public void calcularPromedio() {
+
+        JOptionPane.showMessageDialog(
+                null,"Aqui intenten cuadrar las cosas que use revisen el Promedio.java"
+        );
+    }
+
+    public Estudiante[] getEstudiantes() {
+        return estudiantes;
+    }
+
+    public Materia[] getMaterias() {
+        return materias;
+    }
+
+    public int getCantidadEstudiantes() {
+        return cantidadEstudiantes;
+    }
+
+    public int getCantidadMaterias() {
+        return cantidadMaterias;
+    }
+
+    public static void main(String[] args) {
+
+        MenuPrincipal menu = new MenuPrincipal();
+
+        menu.mostrarMenu();
+    }
+}
