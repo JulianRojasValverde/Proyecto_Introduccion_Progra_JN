c342107318c0c3d9d47ca7922881d3b4555d0423
BrianPicado <bpicado50144@ufide.ac.cr>
2026-07-28 23:21:17 -0600

    Parte brian

---
+++ Proyecto_Introduccion_Programacion_JN/src/main/java/com/mycompany/proyecto_introduccion_programacion_jn/Materia.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_introduccion_progra_jn;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brian
+ */
+public class Materia {
+
+    private String nombreMateria;
+    private String codigoMateria;
+    private Estudiante estudiante;
+
+    public Materia() {
+        nombreMateria = "";
+        codigoMateria = "";
+        estudiante = null;
+    }
+
+    public Materia(
+            String nombreMateria,
+            String codigoMateria,
+            Estudiante estudiante) {
+
+        this.nombreMateria = nombreMateria;
+        this.codigoMateria = codigoMateria;
+        this.estudiante = estudiante;
+    }
+
+    public void registrarMateria(Estudiante estudianteSeleccionado) {
+
+        estudiante = estudianteSeleccionado;
+
+        nombreMateria = JOptionPane.showInputDialog(
+                null,"Ingrese el nombre de la materia:"
+        );
+
+        codigoMateria = JOptionPane.showInputDialog(
+                null,"Ingrese el código de la materia:"
+        );
+
+        JOptionPane.showMessageDialog(
+                null,"Materia registrada correctamente para:\n"
+                + estudiante.getNombre()
+        );
+    }
+
+    public String mostrarDatos() {
+
+        String informacion;
+
+        informacion = "Nombre de la materia: " + nombreMateria
+                + "\nCódigo de la materia: " + codigoMateria
+                + "\nEstudiante: " + estudiante.getNombre()
+                + "\nCédula: " + estudiante.getCedula();
+
+        return informacion;
+    }
+
+    public String getNombreMateria() {
+        return nombreMateria;
+    }
+
+    public void setNombreMateria(String nombreMateria) {
+        this.nombreMateria = nombreMateria;
+    }
+
+    public String getCodigoMateria() {
+        return codigoMateria;
+    }
+
+    public void setCodigoMateria(String codigoMateria) {
+        this.codigoMateria = codigoMateria;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+}
